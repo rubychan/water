4f350d1901793ed9fde74c29e6f2d3637c093568
Kornelius Kalnbach <murphy@rubychan.de>
2011-06-10 23:33:04 +0200

    code refac

--- lib/water.rb
+++ lib/water.rb
@@ -1,16 +1,32 @@
 require 'coderay'
 require 'launchy'
 
-module Water
+class Water
   DIFF_FILE_NAME = 'diff.html'
   
   def self.run
-    # FIXME: Don't just write into the currend folder.
-    File.open DIFF_FILE_NAME, 'w' do |file|
-      file.write CodeRay.scan(ARGF.read, :diff).page
-    end
+    new.run
+  end
+  
+  def run
+    @diff = ARGF.read
     
-    url = "file://#{File.join Dir.pwd, DIFF_FILE_NAME}"
-    Launchy.open url
+    if @diff.chomp.empty?
+      puts 'Your diff is empty.'
+    else
+      write_diff_file
+      open_diff_file
+    end
+  end
+  
+  def write_diff_file
+    # FIXME: Don't just write into the current folder.
+    File.open DIFF_FILE_NAME, 'w' do |file|
+      file.write CodeRay.scan(@diff, :diff).page
+    end
+  end
+  
+  def open_diff_file
+    Launchy.open "file://#{File.join Dir.pwd, DIFF_FILE_NAME}"
   end
 end
--- water.gemspec
+++ water.gemspec
@@ -3,7 +3,7 @@ $:.push File.expand_path("../lib", __FILE__)
 
 Gem::Specification.new do |s|
   s.name        = "water"
-  s.version     = '0.2'
+  s.version     = '0.2.1'
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Kornelius Kalnbach"]
   s.email       = ["murphy@rubychan.de"]
